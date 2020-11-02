692acb6a24ad7f7b50783503f6035023da3395a7
Luis Sanchez <thepipo2698@gmail.com>
2020-11-02 10:17:46 +0100

    morgan is now out of the way

--- database.js
+++ database.js
@@ -1,17 +1,9 @@
 const Sequelize = require('sequelize');
 
+console.log(process.env)
+
 if (process.env.NODE_ENV === 'production'){
-  const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    host: 'localhost',
-    dialect:  "postgres",
-    pool: {
-      max: 5,
-      min: 0,
-      require: 30000,
-      idle: 10000
-    },
-    logging: false
-  });
+  const sequelize = new Sequelize('postgres://zncfdsmuohggvr:ef6b136cd4b66021ab2666f98821fcc84b0a3b89c48c9cc122587465da9d90a1@ec2-34-232-24-202.compute-1.amazonaws.com:5432/d4283chjodv053');
 
   module.exports = sequelize;
 
