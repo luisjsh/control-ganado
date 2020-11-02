9a7de77c1d9bc13f85f89137c5f6983a975af228
Luis Sanchez <thepipo2698@gmail.com>
2020-11-02 07:48:55 +0100

    commit!

--- database.js
+++ database.js
@@ -1,9 +1,10 @@
 const Sequelize = require('sequelize');
 
+const env = require('./env')
 
- const sequelize = new Sequelize("takos", "postgres", "12345", {
-    host: "localhost",
-    dialect: "postgres",
+ const sequelize = new Sequelize(env.db, env.username, env.password, {
+    host: env.host,
+    dialect: env.dialect,
     pool: {
       max: 5,
       min: 0,
---
+++ env.js
@@ -0,0 +1,8 @@
+module.exports = {
+    db: 'd27r0832pc57ar',
+    username: 'gnrhqmvvkwqfpm',
+    password: '45ef3ccc83ab3ab941640b0c98a5a9a11ea7b1781ec63e2a607c13f8dfdf88e1',
+    host: 'ec2-54-156-85-145.compute-1.amazonaws.com',
+    port: 5432,
+    dialect: 'postgres'
+}
