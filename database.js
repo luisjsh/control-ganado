91995befdcf646c984a582dd77603f4c14a850a9
Luis Sanchez <thepipo2698@gmail.com>
2020-11-18 00:10:38 +0100

    ready to be deployed =D

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
 if (process.env.NODE_ENV === 'production'){
-  const sequelize = new Sequelize('postgres://dzxozqsavhkivq:460708ea68b66efa067c4bd66b86e86ad3bf7248ef87643ccfdd73a08aeb5b5d@ec2-34-232-24-202.compute-1.amazonaws.com:5432/d1vvrpbq7an100');
+  const sequelize = new Sequelize('postgres://cthggvurgaxqyd:cb605057e94670ed6720b0b1243df8503fd138ebce48e1f74622d0ba4ecc6d71@ec2-54-156-149-189.compute-1.amazonaws.com:5432/d46tu2c1t62rg3');
 
   module.exports = sequelize;
 
